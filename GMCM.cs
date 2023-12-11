1e5447ea9b2b3b5535e940ee28b18cf69d4b76e7
KediDili <78353874+KediDili@users.noreply.github.com>
2023-12-11 17:45:54 +0300

    uploading existing files

---
+++ GMCM.cs
@@ -0,0 +1,16 @@
+﻿using StardewModdingAPI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FurnitureTweaks
+{
+    public interface IGMCM
+    {
+        void Register(IManifest mod, Action reset, Action save, bool titleScreenOnly = false);
+
+        void AddBoolOption(IManifest mod, Func<bool> getValue, Action<bool> setValue, Func<string> name, Func<string> tooltip = null, string fieldId = null);
+    }
+}
---
+++ Option.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace FurnitureTweaks
+{
+    public class Option
+    {
+        public bool Enabled = true;
+
+        public Vector2 ToArea;
+    }
+}
